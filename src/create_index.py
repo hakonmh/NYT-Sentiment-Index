7e8609fb65d1c5ee7a37a6dd2b6ddbd30a4650d1
Håkon Magne Holmen <haakonholmen@hotmail.com>
2023-05-25 18:26:25 +0200

    Refactor and document create index functions

--- src/create_index.py
+++ src/create_index.py
@@ -2,74 +2,190 @@ import os
 import pandas as pd
 
 
-def create_index(input_folder='data/classified-nyt-data', output_path='data/nyt-index.csv'):
+def create_sentiment_index(
+    input_folder='data/classified-nyt-data',
+    output_path='data/nyt-index.csv'
+):
+    """Create sentiment index file using classified headlines from input folder.
+
+    Parameters
+    ----------
+    input_folder : str, optional
+        Path to the folder containing CSV files of classified NYT data, by
+        default 'data/classified-nyt-data'.
+    output_path : str, optional
+        Path where the output CSV file will be stored, by default
+        'data/nyt-index.csv'.
+
+    Returns
+    -------
+    pd.DataFrame
+        The index as a pandas DataFrame.
+    """
+    create_index_file(output_path)
+
+    for f in os.listdir(input_folder):
+        file_path = os.path.join(input_folder, f)
+        df = convert_file_to_index(file_path)
+        if df is not None:
+            df.to_csv(output_path, header=False, mode='a')
+
+    index_df = add_smoothed_col_to_index_df(output_path)
+    index_df.to_csv(output_path)
+    return index_df
+
+
+def append_sentiment_index(
+    input_folder='data/classified-nyt-data',
+    output_path='data/nyt-index.csv'
+):
+    """Append new data to the sentiment index file from the given input folder.
+
+    Parameters
+    ----------
+    input_folder : str, optional
+        Path to the folder containing CSV files of classified NYT data, by
+        default 'data/classified-nyt-data'.
+    output_path : str, optional
+        Path where the output CSV file will be stored, by default
+        'data/nyt-index.csv'.
+
+    Returns
+    -------
+    pd.DataFrame
+        The index as a pandas DataFrame.
+    """
+    index_df = pd.read_csv(output_path, index_col=0, parse_dates=True)
+    index_df = index_df[['negative', 'neutral', 'positive', 'total', 'index_value']]
+    append_start_date = index_df.index[-1] + pd.DateOffset(days=1)
+
+    for f in files_missing_in_index(input_folder, append_start_date):
+        file_path = os.path.join(input_folder, f)
+        df = convert_file_to_index(file_path)
+        if df is not None:
+            df = df.loc[append_start_date:]
+            df['smoothed_index_value'] = 0
+            df.to_csv(output_path, header=False, mode='a')
+
+    index_df = add_smoothed_col_to_index_df(output_path)
+    index_df.to_csv(output_path)
+    return index_df
+
+
+def create_index_file(file_path):
+    """Create an empty index file with specified column names.
+
+    Parameters
+    ----------
+    file_path : str
+        Path where the output CSV file will be stored.
+    """
     COLUMN_NAMES = ['date', 'negative', 'neutral', 'positive',
                     'total', 'index_value']
-    pd.DataFrame(columns=COLUMN_NAMES).to_csv(output_path, index=False)
-    for f in os.listdir(input_folder):
-        if f.endswith(".csv"):
-            df = pd.read_csv(
-                os.path.join(input_folder, f), index_col=0,
-                parse_dates=True, na_values=['None']
-            )
-            df = _filter_df(df)
-            try:
-                df = _format_df(df)
-                df = _drop_today_if_in_df(df)  # To avoid writing incomplete data
-                df.to_csv(output_path, header=False, mode='a')
-            except AttributeError as e:
-                print(f'Error in file {f}: {e}')
-                continue
-
-    index = pd.read_csv(output_path, index_col=0, parse_dates=True)
-    index['smoothed_index_value'] = _add_smoothed_index_value(index['index_value'])
-    index.to_csv(output_path)
+    pd.DataFrame(columns=COLUMN_NAMES).to_csv(file_path, index=False)
 
 
-def append_index(input_folder='data/classified-nyt-data', output_path='data/nyt-index.csv'):
-    index = pd.read_csv(output_path, index_col=0, parse_dates=True)
-    index = index[['negative', 'neutral', 'positive', 'total', 'index_value']]
-    append_start_date = index.index[-1] + pd.DateOffset(days=1)
+def files_missing_in_index(input_folder, append_start_date):
+    """Generate a list of files that are missing in the index starting from a given date.
 
-    for f in _files_missing_in_index(input_folder, append_start_date):
-        try:
-            df = pd.read_csv(os.path.join(input_folder, f), index_col=0, parse_dates=True)
-            df = _filter_df(df)
-            df = _format_df(df)
-            df = df.loc[append_start_date:]
-            df = _drop_today_if_in_df(df)
-            index = pd.concat([index, df], axis=0)
-        except AttributeError as e:
-            print(f'Error in file {f}: {e}')
-            continue
+    Parameters
+    ----------
+    input_folder : str
+        Path to the folder containing the input CSV files.
+    append_start_date : datetime
+        The starting date for the files that will be appended.
 
-    index['smoothed_index_value'] = _add_smoothed_index_value(index['index_value'])
-    index.index.name = 'date'
-    index.to_csv(output_path)
-
-
-def _files_missing_in_index(input_folder, append_start_date):
+    Returns
+    -------
+    list
+        List of file names that are missing in the index.
+    """
     files = os.listdir(input_folder)
     files = [f for f in files if f.endswith(".csv")]
     month_start = append_start_date.replace(day=1)
-    files = [f for f in files if __file_to_dtime(f) >= month_start]
+    files = [f for f in files if _file_to_dtime(f) >= month_start]
     return files
 
 
-def __file_to_dtime(file_name):
-    """Convert date string to month and year"""
+def _file_to_dtime(file_name):
+    """Convert the date string in a file name to a datetime object.
+    The datetime object is the first day of the month given in the file name.
+
+    Parameters
+    ----------
+    file_name : str
+        The name of the file to be converted.
+
+    Returns
+    -------
+    datetime
+        The datetime object corresponding to the file name.
+    """
     date_str = file_name.split('.')[0]
     return pd.to_datetime(date_str)
 
 
-def _filter_df(df):
+def convert_file_to_index(file_path):
+    """Convert csv file of classified headlines into an index DataFrame.
+
+    Parameters
+    ----------
+    file_path : str
+        Path to the CSV file to be converted.
+
+    Returns
+    -------
+    DataFrame or None
+        Formatted DataFrame if successful, None if an error occurs.
+    """
+    df = pd.read_csv(file_path, index_col=0, parse_dates=True)
+    try:
+        df = _filter_headlines(df)
+        df = _format_to_index(df)
+        df = _drop_today_if_in_df(df)  # To avoid writing incomplete data
+        return df
+    except AttributeError as e:
+        f = os.path.basename(file_path)
+        print(f'Error in file {f}: {e}')
+        return None
+
+
+def _filter_headlines(df):
+    """Filter DataFrame for 'Economics' headlines longer than 2 words.
+
+    Parameters
+    ----------
+    df : DataFrame
+        The DataFrame to be filtered.
+
+    Returns
+    -------
+    DataFrame
+        The filtered DataFrame.
+    """
     df = df[df['model topic'] == 'Economics']
     # drop headlines with less than 3 words
     df = df[df['headline'].str.split().str.len() >= 3]
     return df
 
 
-def _format_df(df):
+def _format_to_index(df):
+    """Formats DataFrame of classified NYT data into the index format.
+
+    Counts the number of negative, neutral and positive sentiment labels per day and
+    calculates the index value and total number of headlines per day.
+
+    Parameters
+    ----------
+    df : DataFrame
+        The DataFrame to be formatted.
+
+    Returns
+    -------
+    DataFrame
+        The formatted DataFrame.
+    """
+
     df.index.name = 'date'
     df = __count_num_labels_per_day(df)
     df['total'] = df.sum(axis=1)
@@ -79,7 +195,19 @@ def _format_df(df):
 
 
 def __count_num_labels_per_day(df):
-    """Count number of negative, neutral and positive sentiment labels per day"""
+    """Count the number of negative, neutral, and positive sentiment labels per day.
+
+    Parameters
+    ----------
+    df : DataFrame
+        Input DataFrame with a 'sentiment' column and datetime index.
+
+    Returns
+    -------
+    DataFrame
+        DataFrame with date as index and counts of negative, neutral, and positive labels
+        as columns.
+    """
     df = df['sentiment'].groupby(df.index.date).value_counts().unstack()
     df = df.fillna(0)
     for col in ['Negative', 'Neutral', 'Positive']:
@@ -93,7 +221,20 @@ def __count_num_labels_per_day(df):
 
 
 def __resample_to_day(df, index_col):
-    """Resample to daily frequency and fill missing index values with rolling mean"""
+    """Resample the DataFrame to daily freq and fill missing index values with rolling mean.
+
+    Parameters
+    ----------
+    df : DataFrame
+        The DataFrame to be resampled.
+    index_col : str
+        Name of the column to use for filling missing values.
+
+    Returns
+    -------
+    DataFrame
+        The resampled DataFrame.
+    """
     df.index = pd.DatetimeIndex(df.index)
     sma = df[index_col].rolling(365, min_periods=1).mean()
     sma = sma.resample('D').ffill()
@@ -104,23 +245,64 @@ def __resample_to_day(df, index_col):
     return df
 
 
-def _add_smoothed_index_value(index_series):
-    """Calculate smoothed and detrended index value.
-    Uses 100-day EMA as smoothing and 10-year SMA as trend.
-    """
-    TEN_YEARS = 365 * 10
-    smoothed_df = index_series.ewm(span=100).mean()
-    trend_df = index_series.rolling(TEN_YEARS).mean()
-    index = smoothed_df - trend_df + 0.5  # Detrended index
-    return index
-
-
 def _drop_today_if_in_df(df):
-    """Drop today's data if it is in the dataframe.
-    This is to avoid having incomplete data in the index.
+    """Drop today's data from the DataFrame if it exists.
+
+    Parameters
+    ----------
+    df : DataFrame
+        The DataFrame from which today's data
+    should be dropped if it exists.
+
+    Returns
+    -------
+    DataFrame
+        The DataFrame with today's data possibly dropped.
     """
     today = pd.Timestamp.today().floor('D')
     latest_day_is_today = df.index[-1] == today
     if latest_day_is_today:
         df = df.iloc[:-1]
     return df
+
+
+def add_smoothed_col_to_index_df(index_path):
+    """Read index file and add a column of smoothed index values.
+
+    Parameters
+    ----------
+    index_path : str
+        Path to the index CSV file.
+
+    Returns
+    -------
+    DataFrame
+        DataFrame with an added column of smoothed index values.
+    """
+    index_df = pd.read_csv(index_path, index_col=0, parse_dates=True)
+    index_df = index_df[['negative', 'neutral', 'positive', 'total', 'index_value']]
+    index_df['smoothed_index_value'] = __calculate_smoothed_index(index_df['index_value'])
+    index_df.index.name = 'date'
+    return index_df
+
+
+def __calculate_smoothed_index(index_series):
+    """Calculate a smoothed and detrended index value from the given series.
+
+    Uses 100-day EMA as smoothing and 10-year SMA as trend.
+
+    Parameters
+    ----------
+    index_series : Series
+        The series from which to calculate the smoothed and detrended index.
+
+    Returns
+    -------
+    Series
+        The series with the calculated index values.
+    """
+    TEN_YEARS = 365 * 10
+    smoothed_df = index_series.ewm(span=100).mean()
+    trend_df = index_series.rolling(TEN_YEARS).mean()
+    index = smoothed_df - trend_df + 0.5  # Detrended index
+    return index
--- src/main.py
+++ src/main.py
@@ -6,15 +6,15 @@ import create_index
 def make_whole_history():
     download.nyt_download_history(1852, overwrite=False)
     classify.classify_full_history(overwrite=False)
-    create_index.create_index()
+    create_index.create_sentiment_index()
 
 
 def make_latest():
     download.nyt_download_latest()
     classify.classify_latest()
-    create_index.append_index()
+    create_index.append_sentiment_index()
 
 
 if __name__ == "__main__":
-    make_whole_history()
-    # make_latest()
+    # make_whole_history()
+    make_latest()
