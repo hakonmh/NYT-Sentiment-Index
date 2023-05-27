e9fc5b7c45a7d45e2abf8791a543142c7518ebee
Håkon Magne Holmen <haakonholmen@hotmail.com>
2023-05-27 11:36:02 +0200

    Change project version

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ current_folder = os.path.abspath(os.path.dirname(__file__))
 
 setup(
     name='newspulse',
-    version='0.0.1',
-    description='A news sentiment index.',
+    version='0.1.0',
+    description='A daily sentiment index based on New York Times economic news.',
     python_requires='>=3.8',
     packages=find_packages(['tests', 'data'])
 )
