25b8d9d798b4c7712d5ad0181fe9f32b21156301
Håkon Magne Holmen <haakonholmen@hotmail.com>
2023-06-30 18:57:50 +0200

    Update package name and version

--- setup.py
+++ setup.py
@@ -4,9 +4,15 @@ import os
 current_folder = os.path.abspath(os.path.dirname(__file__))
 
 setup(
-    name='newspulse',
-    version='0.1.0',
+    name='newsindex',
+    version='0.1.1',
     description='A daily sentiment index based on New York Times economic news.',
     python_requires='>=3.8',
-    packages=find_packages(['tests', 'data'])
+    packages=find_packages(exclude=['tests', 'data']),
+    install_requires=[
+        'pandas>=1.4.0',
+        'pynytimes',
+        'torch',
+        'transformers',
+    ],
 )
